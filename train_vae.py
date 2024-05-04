08a229c02c5262aa81e9a56ee20c14c9c6d637e0
Miran <miranoezdogan@gmail.com>
2024-05-04 19:26:25 +0100

    pretty up the training script

--- train_vae.py
+++ train_vae.py
@@ -2,32 +2,37 @@ import torch
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from pytorch_lightning import Trainer
+from pytorch_lightning import seed_everything
 from argparse import ArgumentParser
-from models import ConvVAE
+from models import ConvVAEModule
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
-from lightning.pytorch import Trainer, seed_everything
-from datasets import MNISTDebugDataModule as MNISTDataModule #TODO CHANGE
+from datasets import MNISTDataModule, CIFAR10DataModule, CIFAR100DataModule
+#from datasets import MNISTDebugDataModule as MNISTDataModule #TODO CHANGE
+from datasets import DATAMODULES
 import os
+
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="VAE for DDF", description="Train VAE for downstream use with differential decision forests")
-    parser.add_argument("--batch_size", type=int, default=512, help="batch size")
+    parser.add_argument("--batch-size", type=int, default=512, help="batch size")
     parser.add_argument("--epochs", type=int, default=50, help="num epochs")
-    parser.add_argument("--latent-dim", type=int, default=8, help="size of latent dim for our vae")
+    parser.add_argument("--latent-dim", type=int, default=32, help="size of latent dim for our vae")
     parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
-    parser.add_argument("--kl-coeff", type=int, default=5, help="kl coeff aka beta term in the elbo loss function")
-    parser.add_argument("--output_dir", type=str, default=os.path.join("results", 'vae'), help="output directory")
-    parser.add_argument("--anomaly_detect", help="Detect anomalies", action="store_true", default=False)
+    parser.add_argument("--kl-coeff", type=int, default=1, help="kl coeff aka beta term in the elbo loss function")
+    parser.add_argument("--output-dir", type=str, default=os.path.join("results", 'vae'), help="output directory")
+    parser.add_argument("--anomaly-detect", help="Detect anomalies", action="store_true", default=False)
     parser.add_argument("--name", type=str, default="vae-for-ddf", help="wandb name of the run")
     parser.add_argument("--checkpoint", type=str, default=None, help="checkpoint to load")
+    parser.add_argument("--dataset", type=str, default="mnist", help="dataset to use", choices=DATAMODULES.keys())
     args = parser.parse_args()
     seed_everything(42)
     os.makedirs(args.output_dir, exist_ok=True)
     wandb_logger = WandbLogger(
     name=args.name,
     project=parser.prog,
-    save_dir=args.output,
+    save_dir=args.output_dir,
     log_model=True,  # Log checkpoint only at the end of training (to stop my wandb running out of storage!)
     )
     #args without name and output_dir
@@ -57,7 +62,7 @@ if __name__ == "__main__":
     )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = ConvVAE(**model_params)
+    model = ConvVAEModule(**model_params)
     model.to(device)
 
     trainer = Trainer(
@@ -74,6 +79,4 @@ if __name__ == "__main__":
         ckpt_path=args.checkpoint,
         )
     trainer.test(datamodule=datamodule)
-    wandb_logger.save()
-    wandb_logger.finalize()
 
