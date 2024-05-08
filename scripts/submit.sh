8f30b6dc2ef106e2f1216b138152dd37fcc033ff
Miran <miranoezdogan@gmail.com>
2024-05-08 17:53:45 +0100

    enable batch submissions

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -7,10 +7,13 @@
 #SBATCH --partition=short
 #SBATCH --account=engs-pnpl
 #SBATCH --output=slurm_out/%j.out
+#SBATCH --array=1-3
+
+config=$(sed "${SLURM_ARRAY_TASK_ID}q;d" config_list.txt)
 
 module load Anaconda3/2022.10
 conda activate /data/coml-oxmedis/trin4076/arc_nt_env
 
 export WANDB_CACHE_DIR=$DATA/wandb_cache
 
-python train_vae.py
+python $config
